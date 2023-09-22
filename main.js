8adde5dd231a0c47f9008046aaebe87c6df16f29
isabelamendessiuza <131874069+isabelamendessiuza@users.noreply.github.com>
2023-09-22 09:16:33 -0300

    d

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function tocar (idAudio) {
+function tocar(idAudio) {
     document.querySelector('#som_tecla_Toim').play();
 }
 
@@ -7,11 +7,11 @@ const lista = document.querySelector('.tecla');
 let contador = 0;
 
 while (contador < lista.length) {
-    lista[contdor].onclick = function () {
+    lista[contador].onclick = function () {
         tocar('#som_tecla_Toim');
     }
-    const classe = lista [contador].classList[1];
+    const classe = lista[contador].classList[1];
    console.log(classe);
     contador = contador + 1;
-    console.log (contador)
+    console.log(contador);
 }
