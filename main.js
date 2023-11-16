7f907cd502993ef24170db1b27fd9cad822d7d09
isabelamendessiuza <131874069+isabelamendessiuza@users.noreply.github.com>
2023-11-16 07:45:09 -0300

    7

--- main.js
+++ main.js
@@ -29,7 +29,6 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         if (evento.code === 'Space' || evento.code === 'Enter') {
             tecla.classList.add('ativa');
         }
-
     }
 
     tecla.onkeyup = function () {
